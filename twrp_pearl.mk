0e0a4a562fb945ab33d0eeb427cc43592dea72ff
kongdwm <3094744152@qq.com>
2025-01-01 13:50:13 +0800

    up

--- twrp_pearl.mk
+++ twrp_pearl.mk
@@ -5,28 +5,31 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from common AOSP config
+# Configure base.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk) # vab加载到vendor boot里使用
 
-# Inherit from TWRP product configuration
+# Configure core_64_bit.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Configure launch_with_vendor_ramdisk.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Configure twrp config common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device specific configs
 $(call inherit-product, device/xiaomi/pearl/device.mk)
 
+# Device identifier
 PRODUCT_DEVICE := pearl
 PRODUCT_NAME := twrp_pearl
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 23054RA19C
-PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_RELEASE_NAME := Redmi Note 12T Pro
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="pearl-user 12 SP1A.210812.016 V816.0.8.0.ULHCNXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/pearl/pearl:12/SP1A.210812.016/V816.0.8.0.ULHCNXM:user/release-keys
-
+# Hide Reflash TWRP
 PRODUCT_PROPERTY_OVERRIDES += ro.twrp.vendor_boot=true
